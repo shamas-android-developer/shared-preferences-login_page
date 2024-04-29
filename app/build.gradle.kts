fab2d1ffc61eb027f047b3f7b4240412c5292cf6
shamas-android-developer <shamsnisar05@gmail.com>
2024-04-29 04:50:32 -0700

    Modified file's data-

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,23 +51,9 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.support.annotations)
-    implementation(libs.androidx.annotation)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.ui.graphics.android)
+    implementation(libs.androidx.foundation.android)
+    implementation(libs.androidx.material3.android)
 }
--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -4,7 +4,6 @@ package com.example.myapplication
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -18,7 +17,7 @@ class SecondActivity : AppCompatActivity() {
     private lateinit var buttonLogout: Button
     private lateinit var buttonLogInfo: Button
 
-    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
@@ -26,7 +25,7 @@ class SecondActivity : AppCompatActivity() {
         // Initialize view elements
         buttonLogout = findViewById(R.id.buttonLogout)
         buttonLogInfo = findViewById(R.id.buttonLoginInfo)
-        multilineText = findViewById(R.id.editMultiLine)
+        multilineText = findViewById(R.id.editWelcomeText)
 
         welcomeScreen = SharedPref(this)
 
